84057d77601c80202fb181589032f2a59f07a823
Paul Tobin <tobster111@gmail.com>
2024-04-19 15:58:12 +0100

    Add initial files and dependencies for Pulumi AWS TypeScript project

---
+++ resources/bucket.ts
@@ -0,0 +1,36 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+type FMBucketArgs = {
+  Name: string;
+  Product: string;
+};
+
+export class FMBucket extends pulumi.ComponentResource {
+  constructor(args: FMBucketArgs, opts: pulumi.ComponentResourceOptions) {
+    const resourceName = `${args.Product}-${args.Name}`;
+
+    super("pkg:index:FMBucket", resourceName, {}, opts);
+
+    const stack = pulumi.getStack();
+
+    const bucketName = `${resourceName}-${stack}`;
+
+    const bucket = new aws.s3.Bucket(args.Name, {
+      bucket: bucketName,
+      acl: aws.s3.CannedAcl.Private,
+      tags: {
+        Name: bucketName,
+        Environment: stack,
+      },
+    });
+
+    new aws.s3.BucketPublicAccessBlock(args.Name, {
+      bucket: bucket.id,
+      blockPublicAcls: true,
+      blockPublicPolicy: true,
+      ignorePublicAcls: true,
+      restrictPublicBuckets: true,
+    });
+  }
+}
